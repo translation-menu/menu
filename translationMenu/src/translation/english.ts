68eed20c0b9e44ac47bbcda3038f75a5bd3780c5
donaldli2020 <donaldli2020@outlook.com>
2022-02-11 10:47:42 -0500

    Update english.ts

--- translationMenu/src/translation/english.ts
+++ translationMenu/src/translation/english.ts
@@ -1,1 +1,7 @@
+// @ts-nocheck
+import { Swal} from "../utils/swal";
+import { Translation, translate, category} from "../index";
 
+new Translation(category.english, "Detect Language to English").setClick(async () => {
+        translate(auto, en);
+});
